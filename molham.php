32bfe1d365dc1da21e6cf58799f59bbb1846375d
xHurricane <molham.work9@gmail.com>
2021-11-20 20:12:04 +0200

    updated php

--- molham.php
+++ molham.php
@@ -1,10 +1,7 @@
 <?php
-$message_sent = false;
+
 if(isset($_POST['email']) && $_POST['email'] != "") {
-
 if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-
-
 $userName = $_POST['name'];
 $userEmail = $_POST['email'];
 $messageSubject = $_POST['subject'];
@@ -18,17 +15,26 @@ $body .= "Email: ". $userEmail."\r\n";
 $body .= "Message: ". $message."\r\n";
 
 mail($to,$messageSubject,$body);
-
-$message_sent = true;
 }
-
-
-
-
-
 }
-
-
-
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo "<title> Thanks ${$userName} </title>";?>
+</head>
+<body>
+    
+    <?php echo "<h1 id=\"greetingMessage\"> Thank You ${userName} for contacting!</h1>"; ?>
+    <?php echo "<p id=\"actualMessage\"> Your Message Will Look Like This: <br> ${body} </p>"; ?>
+    
+
+
+</body>
+</html>
