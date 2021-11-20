ebed83519c6d7725c02e7a3a171fd92b6c1652a3
xHurricane <molham.work9@gmail.com>
2021-11-20 19:45:30 +0200

    fixed php

--- molham.php
+++ molham.php
@@ -1,5 +1,34 @@
 <?php
+$message_sent = false;
+if(isset($_POST['email']) && $_POST['email'] != "") {
 
-mail($_POST["Email"],$_POST["Subject"],$_POST["Message"])
+if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
 
-; ?>
+
+$userName = $_POST['name'];
+$userEmail = $_POST['email'];
+$messageSubject = $_POST['subject'];
+$message = $_POST['message'];
+
+$to = "molham.work9@gmail.com";
+$body = "";
+
+$body .= "From: ". $userName."\r\n";
+$body .= "Email: ". $userEmail."\r\n";
+$body .= "Message: ". $message."\r\n";
+
+mail($to,$messageSubject,$body);
+
+$message_sent = true;
+}
+
+
+
+
+
+}
+
+
+
+
+?>
