0a739f9942a6d5e894de21d1d80f073e87090633
xHurricane <molham.work9@gmail.com>
2021-11-21 19:47:56 +0200

    added nice effects

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+function App() {
+    document.getElementById("DevHurricane").innerHTML = <Typewriter> 
+
+            onInit={(typewriter) => {
+
+                typewriter.typeString("Dev xHurricane...").start();
+
+            }}
+        
+         </Typewriter>;
+}
+
+
+export default App;
