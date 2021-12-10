482c72b4385e9a5875eeed92dda99a69c50067a3
xHurricane <molham.work9@gmail.com>
2021-12-10 16:02:11 +0200

    Added Template

---
+++ portfolio-template/app.js
@@ -0,0 +1,10 @@
+var dom = document.getElementById("domain").innerHTML = ""
+
+var secretPop = () => {
+
+window.alert("HACKED BY MOLHAM PWNED HAHAHA");
+
+document.getElementById("pride").innerHTML = "HACKED BY MOLHAM PWNED HAHAHA";
+    
+
+}
